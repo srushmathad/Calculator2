4b79f309bf28e2ff84e6acc0452e89be10c08b3c
Shrushti <01fe22bca187@kletech.ac.in>
2024-02-08 11:43:47 +0530

    code related to basic arithemetic ops2

--- calculator.java
+++ calculator.java
@@ -12,6 +12,19 @@ public class calculator{
     float substraction (float operand_1,float operand_2){
     return(operand_1 - operand_2);
 }
+    float square1(float operand_1,){
+        return(operand_1*2);
+    }
+    float square2(float operand_2){
+        return(operand_2*2);
+    }
+        
+    float cube1(float operand_1){
+        return(operand_1*3);
+    }
+    float cube2(float operand_2){
+        return(operand_2*3);
+    }
 
  public static void main(String[] args){
     float operand_1=10;
@@ -25,6 +38,7 @@ public class calculator{
     System.out.println("Result of multiplication is:"+ mul_result);
     float div_result=calc.division(operand_1,operand_2);
     System.out.println("Result of Division is:"+ div_result);
+    
 
  }
 }
